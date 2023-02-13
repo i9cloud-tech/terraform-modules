af15f86bb871f54c545e1500424195bbb4134965
robsonandradev <robsom.s.a@gmail.com>
2023-02-13 20:38:22 -0300

    Add cluster id as output

--- ecs-cluster/output.tf
+++ ecs-cluster/output.tf
@@ -3,6 +3,11 @@ output "ecs_cluster_arn" {
   value       = aws_ecs_cluster.main.arn
 }
 
+output "ecs_cluster_id" {
+  description = "The ARN of the ECS cluster."
+  value       = aws_ecs_cluster.main.id
+}
+
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster."
   value       = aws_ecs_cluster.main.name
